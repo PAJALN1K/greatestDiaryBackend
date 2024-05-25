5640b1e1297b869a3e2c88a67ddd6bc51f0b7acc
PAJALN1K <p4j4ln1k@gmail.com>
2024-05-25 17:44:25 +0300

    first commit

---
+++ src/main/java/com/example/greatestdiarybackend/GreatestDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.greatestdiarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatestDiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatestDiaryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/greatestdiarybackend/GreatestDiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greatestdiarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatestDiaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
